7b111adb0c0d6bc9f0c671091d765a99e1e94c1f
TSUYUSATO Kitsune <make.just.on@gmail.com>
2019-09-21 11:03:28 +0900

    fix(cats): tests for PartialOrder and Order

--- modules/cats/src/test/scala/dali/cats/DerivePartialOrderSuite.scala
+++ modules/cats/src/test/scala/dali/cats/DerivePartialOrderSuite.scala
@@ -6,6 +6,7 @@ import derive.partialOrder._
 
 import _root_.cats.kernel.PartialOrder
 import _root_.cats.kernel.instances.int._
+import _root_.cats.kernel.instances.option._
 import _root_.cats.kernel.laws.discipline.PartialOrderTests
 import minitest.SimpleTestSuite
 
@@ -13,9 +14,9 @@ object DerivePartialOrderSuite extends SimpleTestSuite with DisciplineCheckers {
   implicit def partialOrderList[A: PartialOrder]: PartialOrder[MyList[A]] = derivePartialOrder
   implicit def partialOrderTree[A: PartialOrder]: PartialOrder[MyTree[A]] = derivePartialOrder
 
-  checkAll("PartialOrderTests[Either[Int, Int]]", PartialOrderTests[Either[Int, Int]].eqv)
-  checkAll("PartialOrderTests[(Int, Int)]", PartialOrderTests[(Int, Int)].eqv)
+  checkAll("PartialOrderTests[Either[Int, Int]]", PartialOrderTests[Either[Int, Int]].partialOrder)
+  checkAll("PartialOrderTests[(Int, Int)]", PartialOrderTests[(Int, Int)].partialOrder)
 
-  checkAll("PartialOrderTests[MyList[Int]]", PartialOrderTests[MyList[Int]].eqv)
-  checkAll("PartialOrderTests[MyTree[Int]]", PartialOrderTests[MyTree[Int]].eqv)
+  checkAll("PartialOrderTests[MyList[Int]]", PartialOrderTests[MyList[Int]].partialOrder)
+  checkAll("PartialOrderTests[MyTree[Int]]", PartialOrderTests[MyTree[Int]].partialOrder)
 }
